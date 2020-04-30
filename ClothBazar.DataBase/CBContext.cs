3c19bbd0fc97cb37c40ba32ee23ff32884a6f14d
vinaytiwari264 <vinaytiwari264@gmail.com>
2020-04-30 14:59:49 +0530

    Added Product And Category Entities and Created DataBase

---
+++ ClothBazar.DataBase/CBContext.cs
@@ -0,0 +1,23 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.DataBase
+{
+    public  class CBContext : DbContext
+    {
+
+        public CBContext() : base("ClothBazarConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
