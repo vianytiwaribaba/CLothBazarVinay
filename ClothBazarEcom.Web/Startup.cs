c3beb65626aa2a23ad34f53a0dc38e7080a57a52
vinaytiwari264 <vinaytiwari264@gmail.com>
2020-04-30 07:41:07 +0530

    Project Initilised

---
+++ ClothBazarEcom.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothBazarEcom.Web.Startup))]
+namespace ClothBazarEcom.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
